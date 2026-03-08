365b59fe13808242b06e87df3fdf566e50c35356
ridazzahra <5025251065@student.its.ac.id>
2026-03-08 16:10:23 +0700

    Add files via upload

---
+++ primenumbers.cpp
@@ -0,0 +1,32 @@
+#include <stdbool.h>
+#include <bits/stdc++.h>
+using namespace std;
+
+bool isPrime(int n){
+    if(n <= 1)
+        return false;
+    for(int i = 2; i < n; i++){
+        if(n % i == 0)
+            return false;
+    }
+    return true;
+}
+
+void findPrimes(int l, int r){
+    bool found = false;
+    for(int i = l; i <= r; i++){
+        if(isPrime(i)){
+            cout << i << " ";
+            found = true;
+        }
+    }
+    if(!found){
+        cout << "No prime numbers found in the given range.";
+    }
+}
+
+int main(){
+    int l = 10, r = 30;
+    findPrimes(l, r);
+    return 0;
+}
---
+++ string.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+int main(){
+    string str = "GeeksforGeeks";
+    cout << str.size() << endl;
+    return 0;
+}
